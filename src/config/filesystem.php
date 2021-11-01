81b0048201ebf9e8a3c80c20e163c2d08040bd6b
yuanzhihai <396751927@qq.com>
2021-11-01 15:07:49 +0800

    init

--- src/config/filesystem.php
+++ src/config/filesystem.php
@@ -3,55 +3,61 @@
 return [
     'default' => 'local',
     'storage' => [
-        'local' => [
+        'local'  => [
             'driver' => \yzh52521\filesystem\Adapter\LocalAdapter::class,
-            'root' => '',
+            'root'   => '',
+            'url'    => '/'
         ],
         'memory' => [
             'driver' => \yzh52521\filesystem\Adapter\MemoryAdapter::class,
+            'url'    => '/'
         ],
-        's3' => [
-            'driver' => \yzh52521\filesystem\Adapter\S3Adapter::class,
-            'credentials' => [
-                'key' => 'S3_KEY',
+        's3'     => [
+            'driver'                  => \yzh52521\filesystem\Adapter\S3Adapter::class,
+            'credentials'             => [
+                'key'    => 'S3_KEY',
                 'secret' => 'S3_SECRET',
             ],
-            'region' => 'S3_REGION',
-            'version' => 'latest',
-            'bucket_endpoint' => false,
+            'region'                  => 'S3_REGION',
+            'version'                 => 'latest',
+            'bucket_endpoint'         => false,
             'use_path_style_endpoint' => false,
-            'endpoint' => 'S3_ENDPOINT',
-            'bucket_name' => 'S3_BUCKET',
+            'endpoint'                => 'S3_ENDPOINT',
+            'bucket_name'             => 'S3_BUCKET',
+            'url'                     => '/'
         ],
-        'oss' => [
-            'driver' => \yzh52521\filesystem\Adapter\AliyunAdapter::class,
-            'accessId' => 'OSS_ACCESS_ID',
+        'oss'    => [
+            'driver'       => \yzh52521\filesystem\Adapter\AliyunAdapter::class,
+            'accessId'     => 'OSS_ACCESS_ID',
             'accessSecret' => 'OSS_ACCESS_SECRET',
-            'bucket' => 'OSS_BUCKET',
-            'endpoint' => 'OSS_ENDPOINT',
+            'bucket'       => 'OSS_BUCKET',
+            'endpoint'     => 'OSS_ENDPOINT',
+            'url'          => '/'
             // 'timeout' => 3600,
             // 'connectTimeout' => 10,
             // 'isCName' => false,
             // 'token' => null,
             // 'proxy' => null,
         ],
-        'qiniu' => [
-            'driver' => \yzh52521\filesystem\Adapter\QiniuAdapter::class,
+        'qiniu'  => [
+            'driver'    => \yzh52521\filesystem\Adapter\QiniuAdapter::class,
             'accessKey' => 'QINIU_ACCESS_KEY',
             'secretKey' => 'QINIU_SECRET_KEY',
-            'bucket' => 'QINIU_BUCKET',
-            'domain' => 'QINBIU_DOMAIN',
+            'bucket'    => 'QINIU_BUCKET',
+            'domain'    => 'QINBIU_DOMAIN',
+            'url'       => '/'
         ],
-        'cos' => [
-            'driver' => \yzh52521\filesystem\Adapter\CosAdapter::class,
-            'region' => 'COS_REGION',
-            'app_id' => 'COS_APPID',
-            'secret_id' => 'COS_SECRET_ID',
-            'secret_key' => 'COS_SECRET_KEY',
+        'cos'    => [
+            'driver'        => \yzh52521\filesystem\Adapter\CosAdapter::class,
+            'region'        => 'COS_REGION',
+            'app_id'        => 'COS_APPID',
+            'secret_id'     => 'COS_SECRET_ID',
+            'secret_key'    => 'COS_SECRET_KEY',
             // 可选，如果 bucket 为私有访问请打开此项
             // 'signed_url' => false,
-            'bucket' => 'COS_BUCKET',
+            'bucket'        => 'COS_BUCKET',
             'read_from_cdn' => false,
+            'url'           => '/'
             // 'timeout' => 60,
             // 'connect_timeout' => 60,
             // 'cdn' => '',
