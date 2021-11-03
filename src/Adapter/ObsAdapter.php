cc47503368f1cef724436cae64ed3f46afde3a76
yuanzhihai <396751927@qq.com>
2021-11-03 15:07:30 +0800

    add huawei-obs

---
+++ src/Adapter/ObsAdapter.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yzh52521\Filesystem\Adapter;
+
+use yzh52521\Filesystem\Contract\AdapterInterface;
+
+class ObsAdapter implements AdapterInterface
+{
+    public function createAdapter(array $options)
+    {
+        return new yzh52521\Filesystem\Obs\ObsAdapter($options);
+    }
+}
--- src/config/filesystem.php
+++ src/config/filesystem.php
@@ -4,16 +4,16 @@ return [
     'default' => 'local',
     'storage' => [
         'local'  => [
-            'driver' => \yzh52521\filesystem\Adapter\LocalAdapter::class,
+            'driver' => \yzh52521\Filesystem\Adapter\LocalAdapter::class,
             'root'   => '',
             'url'    => '/'
         ],
         'memory' => [
-            'driver' => \yzh52521\filesystem\Adapter\MemoryAdapter::class,
+            'driver' => \yzh52521\Filesystem\Adapter\MemoryAdapter::class,
             'url'    => '/'
         ],
         's3'     => [
-            'driver'                  => \yzh52521\filesystem\Adapter\S3Adapter::class,
+            'driver'                  => \yzh52521\Filesystem\Adapter\S3Adapter::class,
             'credentials'             => [
                 'key'    => 'S3_KEY',
                 'secret' => 'S3_SECRET',
@@ -27,7 +27,7 @@ return [
             'url'                     => '/'
         ],
         'oss'    => [
-            'driver'       => \yzh52521\filesystem\Adapter\AliyunAdapter::class,
+            'driver'       => \yzh52521\Filesystem\Adapter\AliyunAdapter::class,
             'accessId'     => 'OSS_ACCESS_ID',
             'accessSecret' => 'OSS_ACCESS_SECRET',
             'bucket'       => 'OSS_BUCKET',
@@ -40,7 +40,7 @@ return [
             // 'proxy' => null,
         ],
         'qiniu'  => [
-            'driver'    => \yzh52521\filesystem\Adapter\QiniuAdapter::class,
+            'driver'    => \yzh52521\Filesystem\Adapter\QiniuAdapter::class,
             'accessKey' => 'QINIU_ACCESS_KEY',
             'secretKey' => 'QINIU_SECRET_KEY',
             'bucket'    => 'QINIU_BUCKET',
@@ -48,7 +48,7 @@ return [
             'url'       => '/'
         ],
         'cos'    => [
-            'driver'        => \yzh52521\filesystem\Adapter\CosAdapter::class,
+            'driver'        => \yzh52521\Filesystem\Adapter\CosAdapter::class,
             'region'        => 'COS_REGION',
             'app_id'        => 'COS_APPID',
             'secret_id'     => 'COS_SECRET_ID',
@@ -62,6 +62,23 @@ return [
             // 'connect_timeout' => 60,
             // 'cdn' => '',
             // 'scheme' => 'https',
+        ],
+        'obs'    => [
+            'driver'     => \yzh52521\Filesystem\Adapter\ObsAdapter::class,
+            'key'        => 'OBS_ACCESS_ID',
+            // <Your Huawei OBS AccessKeyId>
+            'secret'     => 'OBS_ACCESS_KEY',
+            // <Your Huawei OBS AccessKeySecret>
+            'bucket'     => 'OBS_BUCKET',
+            // <OBS bucket name>
+            'endpoint'   => 'OBS_ENDPOINT',
+            // <the endpoint of OBS, E.g: (https:// or http://).obs.cn-east-2.myhuaweicloud.com | custom domain, E.g:img.abc.com> OBS 外网节点或自定义外部域名
+            'cdn_domain' => 'OBS_CDN_DOMAIN',
+            //<CDN domain, cdn域名> 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn
+            'ssl_verify' => 'OBS_SSL_VERIFY',
+            // <true|false> true to use 'https://' and false to use 'http://'. default is false,
+            'debug'      => 'APP_DEBUG',
+            // <true|false>
         ],
     ],
 ];
